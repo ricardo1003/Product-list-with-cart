ff7669f08e58eaf852cfb331099c0138be9eee28
unknown <ricardodonado1105@gmail.com>
2024-07-15 20:35:52 -0500

    logic of the amount of each item created

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
 const addToCartButtons = document.getElementsByClassName("addToCartButton")
 const decrementButtons = document.getElementsByClassName("decrement")
+const amountOfItems = document.getElementsByClassName("amount")
 const incrementButtons = document.getElementsByClassName("increment")
 
 
 for(let i=0;i<addToCartButtons.length;i++){
     addToCartButtons[i].addEventListener("click",()=>{
         addToCartButtons[i].classList.add("clicked")
-        // addToCartButtons[i].innerHTML =
+        amountOfItems[i].innerHTML = 1
+    })
+    decrementButtons[i].addEventListener("click",()=>{
+        amountOfItems[i].innerHTML = parseInt(amountOfItems[i].innerHTML) - 1
+        if(amountOfItems[i].innerHTML <= 0){
+            addToCartButtons[i].classList.remove("clicked")
+        }
+    })
+    incrementButtons[i].addEventListener("click",()=>{
+        amountOfItems[i].innerHTML = parseInt(amountOfItems[i].innerHTML) + 1
     })
 }
