58102b9430baf9976b07dfe82869b5d4bda7f04d
unknown <ricardodonado1105@gmail.com>
2024-07-15 20:26:17 -0500

    complete HTML restructuration, also eventListener added to the 'Add to cart' buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+const addToCartButtons = document.getElementsByClassName("addToCartButton")
+const decrementButtons = document.getElementsByClassName("decrement")
+const incrementButtons = document.getElementsByClassName("increment")
+
+
+for(let i=0;i<addToCartButtons.length;i++){
+    addToCartButtons[i].addEventListener("click",()=>{
+        addToCartButtons[i].classList.add("clicked")
+        // addToCartButtons[i].innerHTML =
+    })
+}
