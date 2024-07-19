2194a51ed95f6292b377b27ac3ea3388bb9bda78
unknown <ricardodonado1105@gmail.com>
2024-07-18 23:44:56 -0500

    the items added to the cart will appear/dissapear on the cart section

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+const productELements = document.getElementsByClassName("product")
 const addToCartButtons = document.getElementsByClassName("addToCartButton")
 const decrementButtons = document.getElementsByClassName("decrement")
 const amountOfItems = document.getElementsByClassName("amount")
@@ -5,6 +6,23 @@ const incrementButtons = document.getElementsByClassName("increment")
 const totalAmount = document.getElementsByClassName("totalAmount")[0]
 const totalAmountNumber = document.getElementsByClassName("totalNumber")[0]
 const yourCartElement = document.getElementsByClassName("cart")[0]
+const productListElement = document.getElementsByClassName("productsAdded")[0]
+
+
+let quantity = 1
+const createProductItem = (name, price) => `
+    <div>
+        <h4>${name}</h4>
+        <div>
+        <p>${quantity}x</p>
+        <p>@$${price} <b>$${price}</b></p>
+        </div>
+    </div>
+    <div class="removeProduct">
+        <img src="./assets/images/icon-remove-item.svg" alt="remove">
+    </div>
+`;
+
 
 const itemNames = []
 const itemPrices = []
@@ -21,8 +39,9 @@ fetch('data.json')
   .catch(error => console.error('Error al cargar el JSON:', error));
 
 
-let totalAmountOfDesserts = 0
-
+  let totalAmountOfDesserts = 0
+  
+const indexOfitem = []
 for(let i=0;i<addToCartButtons.length;i++){
     addToCartButtons[i].addEventListener("click",()=>{
         addToCartButtons[i].classList.add("clicked")
@@ -31,24 +50,40 @@ for(let i=0;i<addToCartButtons.length;i++){
         amountOfItems[i].innerHTML = 1
         totalAmountOfDesserts = parseInt(totalAmountNumber.innerHTML) + 1
         totalAmountNumber.innerHTML = totalAmountOfDesserts
+
+        const newItemAdded = document.createElement("li")
+
+        newItemAdded.classList.add("productAdded")
+        newItemAdded.classList.add (`itemAddedNo.${i}`)
+
+        indexOfitem[i] = document.getElementsByClassName(`itemAddedNo.${i}`)
+
+        newItemAdded.innerHTML = createProductItem(itemNames[i], itemPrices[i])
+
+        productListElement.appendChild(newItemAdded)
     })
      decrementButtons[i].addEventListener("click",()=>{
          amountOfItems[i].innerHTML = parseInt(amountOfItems[i].innerHTML) - 1
          totalAmountOfDesserts += -1
          if(amountOfItems[i].innerHTML <= 0){
-             addToCartButtons[i].classList.remove("clicked")
+            addToCartButtons[i].classList.remove("clicked")
+
+            indexOfitem[i][0].remove()
          }
          if(totalAmountOfDesserts === 0){
              yourCartElement.classList.add("notfilledCart")
              yourCartElement.classList.remove("filledCart")
+
          }
          totalAmountNumber.innerHTML = totalAmountOfDesserts
      })
      incrementButtons[i].addEventListener("click",()=>{
          yourCartElement.classList.remove("notfilledCart")
          yourCartElement.classList.add("filledCart")
+
          amountOfItems[i].innerHTML = parseInt(amountOfItems[i].innerHTML) + 1
          totalAmountOfDesserts += 1
          totalAmountNumber.innerHTML = totalAmountOfDesserts
+
      })
 }
