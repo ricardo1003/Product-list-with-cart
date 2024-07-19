922f09bf6a44df7722e2b223d0a8987b5265c53e
unknown <ricardodonado1105@gmail.com>
2024-07-18 22:27:03 -0500

    JSON data imported, correctly

--- js/scripts.js
+++ js/scripts.js
@@ -6,30 +6,17 @@ const totalAmount = document.getElementsByClassName("totalAmount")[0]
 const totalAmountNumber = document.getElementsByClassName("totalNumber")[0]
 const yourCartElement = document.getElementsByClassName("cart")[0]
 
+const itemNames = []
+const itemPrices = []
+
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    console.log(data[0].name);
 
-    const item1Name = data[0].name
-    const item2Name = data[1].name
-    const item3Name = data[2].name
-    const item4Name = data[3].name
-    const item5Name = data[4].name
-    const item6Name = data[5].name
-    const item7Name = data[6].name
-    const item8Name = data[7].name
-    const item9Name = data[8].name
-
-    const item1Price = data[0].price
-    const item2Price = data[1].price
-    const item3Price = data[2].price
-    const item4Price = data[3].price
-    const item5Price = data[4].price
-    const item6Price = data[5].price
-    const item7Price = data[6].price
-    const item8Price = data[7].price
-    const item9Price = data[8].price
+    data.forEach((item, index) => {
+        itemNames[index] = item.name
+        itemPrices[index] = item.price
+    });
   })
   .catch(error => console.error('Error al cargar el JSON:', error));
 
