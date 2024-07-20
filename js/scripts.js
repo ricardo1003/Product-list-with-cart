216176ebf84a7a65502384fce383f4801c1dff88
unknown <ricardodonado1105@gmail.com>
2024-07-19 22:47:03 -0500

    prices + amount of each unit now displays correctly

--- js/scripts.js
+++ js/scripts.js
@@ -1,89 +1,98 @@
-const productELements = document.getElementsByClassName("product")
+const productElements = document.getElementsByClassName("product")
 const addToCartButtons = document.getElementsByClassName("addToCartButton")
 const decrementButtons = document.getElementsByClassName("decrement")
 const amountOfItems = document.getElementsByClassName("amount")
 const incrementButtons = document.getElementsByClassName("increment")
-const totalAmount = document.getElementsByClassName("totalAmount")[0]
 const totalAmountNumber = document.getElementsByClassName("totalNumber")[0]
 const yourCartElement = document.getElementsByClassName("cart")[0]
 const productListElement = document.getElementsByClassName("productsAdded")[0]
+const totalPriceElement = document.getElementsByClassName("totalPrice")[0]
 
 
-let quantity = 1
-const createProductItem = (name, price) => `
-    <div>
-        <h4>${name}</h4>
-        <div>
-        <p>${quantity}x</p>
-        <p>@$${price} <b>$${price}</b></p>
-        </div>
+let OrderTotal = 0
+const createProductItem = (name, price, quantity) => `
+<div>
+    <h4>${name}</h4>
+    <div class="price">
+        <p><b>${quantity}x</b> @$${price} <b class="unitsPriceSum">$${(price * quantity).toFixed(2)}</b></p>
     </div>
-    <div class="removeProduct">
-        <img src="./assets/images/icon-remove-item.svg" alt="remove">
-    </div>
+</div>
+<div class="removeProduct">
+    <img src="./assets/images/icon-remove-item.svg" alt="remove">
+</div>
 `;
 
-
-const itemNames = []
-const itemPrices = []
+const itemNames = [];
+const itemPrices = [];
 
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-
     data.forEach((item, index) => {
-        itemNames[index] = item.name
-        itemPrices[index] = item.price
+      itemNames[index] = item.name;
+      itemPrices[index] = item.price;
     });
   })
   .catch(error => console.error('Error al cargar el JSON:', error));
 
+let totalAmountOfDesserts = 0;
+const indexOfItem = [];
+const quantities = Array(addToCartButtons.length).fill(0);
 
-  let totalAmountOfDesserts = 0
-  
-const indexOfitem = []
-for(let i=0;i<addToCartButtons.length;i++){
-    addToCartButtons[i].addEventListener("click",()=>{
-        addToCartButtons[i].classList.add("clicked")
-        yourCartElement.classList.remove("notfilledCart")
-         yourCartElement.classList.add("filledCart")
-        amountOfItems[i].innerHTML = 1
-        totalAmountOfDesserts = parseInt(totalAmountNumber.innerHTML) + 1
-        totalAmountNumber.innerHTML = totalAmountOfDesserts
+for (let i = 0; i < addToCartButtons.length; i++) {
+  addToCartButtons[i].addEventListener("click", () => {
+    if (quantities[i] === 0) {
+      quantities[i] = 1;
+      addToCartButtons[i].classList.add("clicked");
+      const newItemAdded = document.createElement("li");
+      newItemAdded.classList.add("productAdded");
+      newItemAdded.dataset.index = i;
+      newItemAdded.innerHTML = createProductItem(itemNames[i], itemPrices[i], quantities[i]);
+      productListElement.appendChild(newItemAdded);
+    } else {
+      quantities[i]++;
+      const itemElement = productListElement.querySelector(`[data-index="${i}"]`);
+      itemElement.innerHTML = createProductItem(itemNames[i], itemPrices[i], quantities[i]);
+    }
 
-        const newItemAdded = document.createElement("li")
+    totalAmountOfDesserts++;
+    totalAmountNumber.innerHTML = totalAmountOfDesserts;
+    yourCartElement.classList.remove("notfilledCart");
+    yourCartElement.classList.add("filledCart");
 
-        newItemAdded.classList.add("productAdded")
-        newItemAdded.classList.add (`itemAddedNo.${i}`)
+    amountOfItems[i].innerHTML = 1
 
-        indexOfitem[i] = document.getElementsByClassName(`itemAddedNo.${i}`)
+  });
+  decrementButtons[i].addEventListener("click", () => {
+    amountOfItems[i].innerHTML--
+    if (quantities[i] > 0) {
+      quantities[i]--;
+      totalAmountOfDesserts--;
 
-        newItemAdded.innerHTML = createProductItem(itemNames[i], itemPrices[i])
+      if (quantities[i] === 0) {
+        addToCartButtons[i].classList.remove("clicked");
+        const itemElement = productListElement.querySelector(`[data-index="${i}"]`);
+        itemElement.remove();
+      } else {
+        const itemElement = productListElement.querySelector(`[data-index="${i}"]`);
+        itemElement.innerHTML = createProductItem(itemNames[i], itemPrices[i], quantities[i]);
+      }
 
-        productListElement.appendChild(newItemAdded)
-    })
-     decrementButtons[i].addEventListener("click",()=>{
-         amountOfItems[i].innerHTML = parseInt(amountOfItems[i].innerHTML) - 1
-         totalAmountOfDesserts += -1
-         if(amountOfItems[i].innerHTML <= 0){
-            addToCartButtons[i].classList.remove("clicked")
-
-            indexOfitem[i][0].remove()
-         }
-         if(totalAmountOfDesserts === 0){
-             yourCartElement.classList.add("notfilledCart")
-             yourCartElement.classList.remove("filledCart")
-
-         }
-         totalAmountNumber.innerHTML = totalAmountOfDesserts
-     })
-     incrementButtons[i].addEventListener("click",()=>{
-         yourCartElement.classList.remove("notfilledCart")
-         yourCartElement.classList.add("filledCart")
-
-         amountOfItems[i].innerHTML = parseInt(amountOfItems[i].innerHTML) + 1
-         totalAmountOfDesserts += 1
-         totalAmountNumber.innerHTML = totalAmountOfDesserts
-
-     })
+      if (totalAmountOfDesserts === 0) {
+        yourCartElement.classList.add("notfilledCart");
+        yourCartElement.classList.remove("filledCart");
+      }
+      totalAmountNumber.innerHTML = totalAmountOfDesserts;
+    }
+  });
+  incrementButtons[i].addEventListener("click", () => {
+    amountOfItems[i].innerHTML++
+    quantities[i]++;
+    totalAmountOfDesserts++;
+    const itemElement = productListElement.querySelector(`[data-index="${i}"]`);
+    itemElement.innerHTML = createProductItem(itemNames[i], itemPrices[i], quantities[i]);
+    totalAmountNumber.innerHTML = totalAmountOfDesserts;
+    yourCartElement.classList.remove("notfilledCart");
+    yourCartElement.classList.add("filledCart");
+  });
 }
